804df80641981decdb0f2240465809ec6fc4a3d6
Julian Kyer <julian.kyer@gmail.com>
2017-02-26 14:35:27 -0700

    Start testing suite

--- test/index.js
+++ test/index.js
@@ -1,6 +1,34 @@
-var expect = require('chai').assert;
-const React  = require('react');
-const App =  require('../app/components/App')
-const LikesCounter = require('../app/components/LikesCounter')
-const ActionButton = require('../app/components/ActionButton')
-import { shallow, mount, render } from 'enzyme'
+import { assert } from 'chai';
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Main from '../lib/components/Main';
+import Controls from '../lib/components/Controls';
+import SevenHour from '../lib/components/SevenHour';
+
+require('locus');
+
+describe('testing weathrly', ()=> {
+  it('should have a component called controls', ()=> {
+    const wrapper = shallow(<Main/>);
+    assert.equal(wrapper.find('Controls').length, 1);
+  });
+  it('should have a component called SevenHour', ()=> {
+    const wrapper = shallow(<Controls/>);
+    assert.equal(wrapper.find('SevenHour').length, 1);
+  });
+  it('should have a default state', ()=> {
+    const wrapper = shallow(<Controls/>);
+    assert.deepEqual(wrapper.state('locationInputField'), '[]');
+    assert.deepEqual(wrapper.state('locationResults'), {});
+  });
+  it.skip('should allow user to input a search', ()=> {
+    const wrapper = shallow(<Controls/>);
+    assert.deepEqual()
+  });
+  it('should have a handle click function', ()=> {
+    const wrapper = shallow(<Controls/>);
+    console.log(wrapper.handleClickEvent);
+  })
+})
+
+
