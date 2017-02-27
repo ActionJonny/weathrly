319f99999474848f168d9997b66922830610a448
Julian Kyer <julian.kyer@gmail.com>
2017-02-27 10:22:06 -0700

    Add tests for Weather

--- test/index.js
+++ test/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Main from '../lib/components/Main';
 import Controls from '../lib/components/Controls';
+import Weather from '../lib/components/Weather';
 import SevenHour from '../lib/components/SevenHour';
-import dataCleaner from '../lib/helpers/dataCleaner';
+// import SevenHourCard from '..lib/components/SevenHourCard';
 import sevenHourCleaner from '../lib/helpers/sevenHourCleaner';
+import dataCleaner from '../lib/helpers/dataCleaner';
 import tenDayCleaner from '../lib/helpers/tenDayCleaner';
-// import  testObject from './test-object.json';
+import  testObject from './test-object';
 
 require('locus');
 
 describe('testing weathrly', ()=> {
-  
-  // const dataObject = JSON.parse(testObject);
-  
+    
   it('Main should have a component called controls', ()=> {
     const wrapper = shallow(<Main/>);
     assert.equal(wrapper.find('Controls').length, 1);
@@ -51,11 +51,39 @@ describe('testing weathrly', ()=> {
     assert.equal(wrapper.find('.header').children('.inputs').children('.searchInput').length, 1);
     assert.equal(wrapper.find('.header').children('.inputs').children('.searchSubmit').length, 1);
   });
-  it.only('Controls should have weather component', ()=> {
-    const wrapper = shallow(<Controls/>);
+  it('Weather should have present city and date', ()=> {
+    const dataObject = testObject;
+    const locationResults = dataCleaner(dataObject);
+    const wrapper = shallow(<Weather locationResults={locationResults}/>);
     
-    console.log(wrapper.find(<Weather/>).length)
+    assert.equal(wrapper.find('.cityName').text(), 'Boulder, CO');
+    assert.equal(wrapper.find('.currentDay').text(), 'February 27');
   });
-})
+  it('Weather should display current, high, and low temp', ()=> {
+    const dataObject = testObject;
+    const locationResults = dataCleaner(dataObject);
+    const wrapper = shallow(<Weather locationResults={locationResults}/>);
+    
+    assert.equal(wrapper.find('.currentTemp').text(), '24.8°');
+    assert.equal(wrapper.find('.highLowPair').text(), 'H 52°L 29°');
+  });
+  it('Weather should display a description of the forecast', ()=> {
+    const dataObject = testObject;
+    const locationResults = dataCleaner(dataObject);
+    const wrapper = shallow(<Weather locationResults={locationResults}/>);
+
+    assert.equal(wrapper.find('.longCond').text(), 'Sunshine and a few clouds. High 52F. NE winds shifting to W at 10 to 20 mph.');
+  });
+  it('Controls should have a header for the ten day forecast', ()=> {
+    const wrapper = shallow(<Controls/>);
+    assert.equal(wrapper.find('h2').text(), 'Seven Hour Forecast')
+  });
+  // it.only('SevenHour should return seven hour cards', ()=> {
+  //   const dataObject = testObject;
+  //   const locationResults = dataCleaner(dataObject);
+  //   const wrapper = mount(<SevenHour locationResults={locationResults}/>);
+  //   console.log(wrapper);
+  // })
+});
 
 
